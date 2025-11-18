80ddeb373a4c6bcb01b7affe0486a50813938355
Nacho Dobles <nacho@Estella.>
2025-11-18 14:52:08 -0600

    Get basic server running, and add gitignore

---
+++ src/main/java/com/backcountry/BackcountryApplication.java
@@ -0,0 +1,12 @@
+package com.backcountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackcountryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackcountryApplication.class, args);
+    }
+}
